d786f1a1fc92ba30ebba51825530c7e46ec5a9d8
Megham Garg <gargmegham3@gmail.com>
2023-09-22 23:19:39 +0530

    updated color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,15 @@ module.exports = {
     extend: {
       colors: {
         servcy: "#4D7E3E",
+        "servcy-green": "#032123",
         "servcy-dark": "#26542F",
         "servcy-light": "#5BA959",
         "servcy-gray": "#DFE0EC",
         "servcy-silver": "#7E84A3",
         "servcy-white": "#F7F6FA",
         "servcy-black": "#2B3232",
+        "servcy-cream": "#F1F2EF",
+        "servcy-wheat": "#D1BFAE",
       },
     },
   },
