31cb4b2e0ede11e5e827cca9e02e83e4ed625775
Megham Garg <gargmegham3@gmail.com>
2023-12-30 14:45:48 +0530

    Add SlackMessageProps and SlackMessageElementProps interfaces

--- app/components/Inbox/SlackMessage.tsx
+++ app/components/Inbox/SlackMessage.tsx
@@ -5,44 +5,7 @@ import cn from "classnames";
 import Image from "next/image";
 import { HiExternalLink, HiPaperClip } from "react-icons/hi";
 
-interface SlackMessageStyleProps {
-  bold?: boolean;
-  italic?: boolean;
-  strike?: boolean;
-  code?: boolean;
-}
-
-interface SlackMessageElementProps {
-  type: string;
-  style?: SlackMessageStyleProps | string;
-  text?: string;
-  indent?: number;
-  border?: boolean;
-  url?: string;
-  user_id?: string;
-  elements?: SlackMessageElementProps[];
-}
-
-interface SlackMessageProps {
-  event_ts: string;
-  type: string;
-  user?: string;
-  ts: string;
-  channel: string;
-  files?: {
-    id: string;
-    url_private_download: string;
-    name: string;
-    url_private: string;
-    title: string;
-  }[];
-  text?: string;
-  blocks?: {
-    type: string;
-    block_id: string;
-    elements: SlackMessageElementProps[];
-  }[];
-}
+import { SlackMessageElementProps, SlackMessageProps } from "@/types/slack";
 
 const SlackMessage = ({
   data,
---
+++ app/types/slack.ts
@@ -0,0 +1,38 @@
+export interface SlackMessageStyleProps {
+  bold?: boolean;
+  italic?: boolean;
+  strike?: boolean;
+  code?: boolean;
+}
+
+export interface SlackMessageElementProps {
+  type: string;
+  style?: SlackMessageStyleProps | string;
+  text?: string;
+  indent?: number;
+  border?: boolean;
+  url?: string;
+  user_id?: string;
+  elements?: SlackMessageElementProps[];
+}
+
+export interface SlackMessageProps {
+  event_ts: string;
+  type: string;
+  user?: string;
+  ts: string;
+  channel: string;
+  files?: {
+    id: string;
+    url_private_download: string;
+    name: string;
+    url_private: string;
+    title: string;
+  }[];
+  text?: string;
+  blocks?: {
+    type: string;
+    block_id: string;
+    elements: SlackMessageElementProps[];
+  }[];
+}
