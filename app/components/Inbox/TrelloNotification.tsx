30b42846fb16c3542c18a235e1565325c230fb48
Megham Garg <gargmegham3@gmail.com>
2024-01-01 15:52:23 +0530

    Add case for "updateCheckItem" in TrelloNotification component

--- app/components/Inbox/TrelloNotification.tsx
+++ app/components/Inbox/TrelloNotification.tsx
@@ -428,6 +428,83 @@ const TrelloNotification = ({ data, cause }: TrelloNotificationProps) => {
           </>
         );
       }
+      case "updateCheckItem": {
+        linkLabel = "View card on Trello";
+        link = data.data.card
+          ? `https://trello.com/c/${data.data.card.shortLink}`
+          : "#null";
+
+        return (
+          <>
+            {data.data.card && (
+              <>
+                <div className="mb-2 flex w-full">
+                  <div className="mr-2 w-[150px] font-mono font-semibold">
+                    Card Name:
+                  </div>
+                  <div>{data.data.card.name}</div>
+                </div>
+                {data.data.checkItem && (
+                  <>
+                    <div className="mb-2 flex w-full">
+                      <div className="mr-2 w-[150px] font-mono font-semibold">
+                        Check Item Name:
+                      </div>
+                      <div>{data.data.checkItem.name}</div>
+                    </div>
+                    <div className="mb-2 flex w-full">
+                      <div className="mr-2 w-[150px] font-mono font-semibold">
+                        Check Item State:
+                      </div>
+                      <div>{data.data.checkItem.state}</div>
+                    </div>
+                  </>
+                )}
+                {data.data.checklist && (
+                  <div className="mb-2 flex w-full">
+                    <div className="mr-2 w-[150px] font-mono font-semibold">
+                      Checklist Name:
+                    </div>
+                    <div>{data.data.checklist.name}</div>
+                  </div>
+                )}
+                {data.data.board && (
+                  <div className="mb-2 flex w-full">
+                    <div className="mr-2 w-[150px] font-mono font-semibold">
+                      Board:
+                    </div>
+                    <div>
+                      {data.data.board.name}{" "}
+                      <a
+                        href={data.data.board.shortLink}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <HiExternalLink
+                          className="my-auto inline text-servcy-silver hover:text-servcy-wheat"
+                          size="18"
+                        />
+                      </a>
+                    </div>
+                  </div>
+                )}
+                {data.data.old && (
+                  <div className="mb-2 flex w-full">
+                    <div className="mr-2 w-[150px] font-mono font-semibold">
+                      Fields Updated:
+                    </div>
+                    <div>
+                      {Object.keys(data.data.old).map((field) => (
+                        <div key={field}>{field}</div>
+                      ))}
+                    </div>
+                  </div>
+                )}
+              </>
+            )}
+          </>
+        );
+      }
       case "deleteCheckItem": {
         linkLabel = "View card on Trello";
         link = data.data.card
