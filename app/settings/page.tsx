b37fa1001fd61b5010580675d0221eda66640ef8
Megham Garg <gargmegham3@gmail.com>
2024-01-02 12:59:29 +0530

    Add integrations and loading state to Settings page

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,16 +1,46 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 // Components
+import { Button, Card, Skeleton } from "antd";
+import Image from "next/image.js";
 import {
   AiFillApi,
+  AiFillSetting,
   AiOutlineArrowRight,
   AiOutlineSetting,
 } from "react-icons/ai";
-import { TbHourglassEmpty } from "react-icons/tb";
+// APIs
+import { fetchIntegrations } from "@/apis/integration";
+// Types
+import { Integration } from "@/types/integration";
 
 export default function Settings(): JSX.Element {
+  const [integrations, setIntegrations] = useState<Integration[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetchIntegrations()
+      .then((integrations) => {
+        setIntegrations(
+          integrations.sort(
+            (a: Integration, b: Integration) =>
+              Number(a.is_wip) - Number(b.is_wip)
+          )
+        );
+      })
+      .catch((error) => {
+        toast.error(error.response.data.detail);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
   const router = useRouter();
+
   return (
     <main className="order-2 h-screen flex-[1_0_16rem] overflow-y-scroll bg-servcy-gray p-3">
       <header className="mb-6 h-[80px] rounded-lg bg-servcy-white p-6">
@@ -27,12 +57,7 @@ export default function Settings(): JSX.Element {
             </div>
             <hr className="h-[1.5px] border-none bg-servcy-wheat" />
             <div className="flex flex-col gap-4 text-sm text-servcy-neutral">
-              <button
-                onClick={() => {
-                  router.push("/settings/integrations");
-                }}
-                className="border-1 flex cursor-pointer flex-row rounded-lg border-servcy-black p-2 hover:border-none hover:bg-servcy-wheat hover:text-servcy-black"
-              >
+              <button className="border-1 flex cursor-pointer flex-row rounded-lg border-servcy-black p-2 hover:border-none hover:bg-servcy-wheat hover:text-servcy-black">
                 <AiFillApi size="18" className="my-auto mr-2" />
                 <p>Integrations</p>
                 <AiOutlineArrowRight size="16" className="my-auto ml-auto" />
@@ -40,13 +65,77 @@ export default function Settings(): JSX.Element {
             </div>
           </div>
         </div>
-        <div className="flex-auto rounded-lg bg-servcy-white p-16">
-          <div className="flex flex-col items-center justify-center">
-            <TbHourglassEmpty className="h-32 w-32 text-servcy-black" />
-            <p className="mt-2 font-semibold text-servcy-black">
-              Please select something from the left menu.
-            </p>
-          </div>
+        <div className="xs:grid-cols-1 grid flex-auto gap-3 rounded-lg bg-servcy-white p-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4">
+          {loading ? (
+            <>
+              <Card className="min-h-[200px] animate-pulse rounded-lg">
+                <Skeleton avatar paragraph={{ rows: 4 }} />
+              </Card>
+              <Card className="min-h-[200px] animate-pulse rounded-lg">
+                <Skeleton avatar paragraph={{ rows: 4 }} />
+              </Card>
+              <Card className="min-h-[200px] animate-pulse rounded-lg">
+                <Skeleton avatar paragraph={{ rows: 4 }} />
+              </Card>
+            </>
+          ) : (
+            integrations
+              .filter(
+                (integration: Integration) =>
+                  integration.account_display_names.length > 0
+              )
+              .map((integration: Integration) => (
+                <Card
+                  key={integration.id}
+                  className="min-h-[200px] cursor-pointer rounded-lg bg-servcy-black text-servcy-white"
+                >
+                  <div className="flex flex-row text-servcy-wheat">
+                    {integration.logo.split(",").map((logo, index) => (
+                      <Image
+                        className="my-auto mr-2 max-h-[40px] min-h-[40px] min-w-[40px] max-w-[40px] rounded-lg border border-servcy-gray bg-servcy-white p-1 last-of-type:mr-5"
+                        src={logo}
+                        width={40}
+                        key={`logo-${index}`}
+                        height={40}
+                        alt={integration.name}
+                      />
+                    ))}
+                    <div className="my-auto flex-col text-lg font-semibold">
+                      {integration.name}
+                    </div>
+                    {integration.configure_at !== "None" &&
+                      integration.account_display_names.length !== 0 && (
+                        <a
+                          href={integration.configure_at}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="my-auto ml-auto hover:text-servcy-light"
+                        >
+                          <AiOutlineSetting
+                            size={20}
+                            color="servcy-white"
+                            className="hover:animate-spin"
+                          />
+                        </a>
+                      )}
+                  </div>
+                  <div className="mt-2 h-16 py-3 pr-3 text-xs max-lg:h-24">
+                    {integration.description}
+                  </div>
+                  <div className="mt-6 flex flex-row justify-between">
+                    <Button
+                      className="!text-servcy-white hover:!border-servcy-wheat hover:!text-servcy-wheat"
+                      size="middle"
+                      onClick={() => router.push(``)}
+                      icon={<AiFillSetting />}
+                      disabled={integration.is_wip}
+                    >
+                      Configure
+                    </Button>
+                  </div>
+                </Card>
+              ))
+          )}
         </div>
       </div>
     </main>
