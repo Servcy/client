9ccdf40914412faea4a73a9dc5f16f63842084bc
Megham Garg <gargmegham3@gmail.com>
2024-01-04 17:51:59 +0530

    Add IntegrationConfigurationModal component

---
+++ app/components/Settings/IntegrationConfigurationModal.tsx
@@ -0,0 +1,26 @@
+// Types
+import { Integration } from "@/types/integration";
+// Components
+import { Modal } from "antd";
+
+export default function IntegrationConfigurationModal({
+  selectedIntegration,
+  onClose,
+}: {
+  selectedIntegration: Integration;
+  onClose: () => void;
+}) {
+  return (
+    <Modal
+      open={true}
+      title={`${selectedIntegration.name} Configuration`}
+      onCancel={onClose}
+      footer={false}
+      width="80vw"
+      bodyStyle={{
+        overflowY: "scroll",
+        overflowX: "hidden",
+      }}
+    ></Modal>
+  );
+}
