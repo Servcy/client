f52dad88f8ca7a79ed7e8e9eacc6a2b54320a427
Megham Garg <gargmegham3@gmail.com>
2024-02-27 11:35:20 +0530

    Refactor getCleanLink function to return clean link without query parameters or fragment

--- app/utils/Shared/index.ts
+++ app/utils/Shared/index.ts
@@ -17,8 +17,10 @@ export function isSmallScreen(): boolean {
 export function getCleanLink(url: string) {
   const anchor = document.createElement("a");
   anchor.href = url;
-  // Input: https://example.com/path/to/resource?param=value#section
-  // Outputs: https://example.com/path/to/resource
-  // Return the clean link (host + pathname) without query parameters, or fragment
+  /*
+    Input: https://example.com/path/to/resource?param=value#section
+    Outputs: https://example.com/path/to/resource
+    Return the clean link (host + pathname) without query parameters, or fragment
+  */
   return anchor.protocol + "//" + anchor.host + anchor.pathname;
 }
