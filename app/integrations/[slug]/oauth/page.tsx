559b7947dbe162c220f4c2a05dd9d60dc3d93939
Megham Garg <gargmegham3@gmail.com>
2024-02-28 12:59:23 +0530

    Remove unused code and update page layout

--- app/integrations/[slug]/oauth/page.tsx
+++ app/integrations/[slug]/oauth/page.tsx
@@ -10,8 +10,6 @@ import { Spin } from "antd";
 import { getQueryParams } from "@/utils/Shared";
 // APIs
 import { integrationOauth as integrationOauthApi } from "@/apis/integration";
-// Context
-import { useSidebarContext } from "@/context/SidebarContext";
 
 const capitalizeFirstLetter = (string: string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
@@ -21,10 +19,8 @@ export default function IntegrationOauth(): JSX.Element {
   const params = useParams();
   const router = useRouter();
   const { slug } = params;
-  const { setIsPageWithSidebar } = useSidebarContext();
 
   useEffect(() => {
-    setIsPageWithSidebar(false);
     if (typeof slug !== "string") return;
     const oauthParams: Record<string, string> = getQueryParams(
       window.location.search
@@ -50,17 +46,15 @@ export default function IntegrationOauth(): JSX.Element {
             }, 1000);
           } else router.push(redirect_uri);
         } else router.push("/integrations");
-        setIsPageWithSidebar(true);
       })
       .catch((error: any) => {
         toast.error(error?.response?.data?.detail || "Something went wrong!");
         router.push("/integrations");
-        setIsPageWithSidebar(true);
       });
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <main className="h-screen w-full bg-servcy-white">
+    <main className="order-2 h-screen w-full bg-servcy-white">
       <div className="flex h-full w-full flex-col items-center justify-center">
         <Spin
           className="m-auto"
