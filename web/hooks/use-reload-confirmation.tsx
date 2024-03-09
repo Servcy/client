220517abe543f3cfdd3465b4e1a967379040b54d
Megham Garg <gargmegham3@gmail.com>
2024-03-09 10:41:57 +0530

    Refactor useReloadConfirmations hook to use new next/navigation hooks

--- web/hooks/use-reload-confirmation.tsx
+++ web/hooks/use-reload-confirmation.tsx
@@ -1,42 +1,26 @@
-import { useRouter } from "next/navigation"
+import { useParams, usePathname } from "next/navigation"
 
 import { useCallback, useEffect, useState } from "react"
 
-//TODO: remove temp flag isActive later and use showAlert as the source of truth
 const useReloadConfirmations = (isActive = true) => {
     const [showAlert, setShowAlert] = useState(false)
-    const router = useRouter()
-
+    const pathname = usePathname()
+    const params = useParams()
     const handleBeforeUnload = useCallback(
         (event: BeforeUnloadEvent) => {
             if (!isActive || !showAlert) return
             event.preventDefault()
+            confirm("Are you sure you want to leave? Changes you made may not be saved.")
             event.returnValue = ""
         },
         [isActive, showAlert]
     )
-
-    const handleRouteChangeStart = useCallback(
-        (url: string) => {
-            if (!isActive || !showAlert) return
-            const leave = confirm("Are you sure you want to leave? Changes you made may not be saved.")
-            if (!leave) {
-                router.events.emit("routeChangeError")
-                throw `Route change to "${url}" was aborted (this error can be safely ignored).`
-            }
-        },
-        [isActive, showAlert, router.events]
-    )
-
     useEffect(() => {
         window.addEventListener("beforeunload", handleBeforeUnload)
-        router.events.on("routeChangeStart", handleRouteChangeStart)
-
         return () => {
             window.removeEventListener("beforeunload", handleBeforeUnload)
-            router.events.off("routeChangeStart", handleRouteChangeStart)
         }
-    }, [handleBeforeUnload, handleRouteChangeStart, router.events])
+    }, [handleBeforeUnload, pathname, params])
 
     return { setShowAlert }
 }
