86e58801b04861751be415c13bf437fadc6958cd
Megham Garg <gargmegham3@gmail.com>
2024-03-12 11:57:23 +0530

    Fix bug in login functionality

--- web/wrappers/DefaultWrapper.tsx
+++ web/wrappers/DefaultWrapper.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link"
-import { useRouter } from "next/navigation"
+import { useParams, useRouter } from "next/navigation"
 
-import { FC, ReactNode, useEffect, useRef, useState } from "react"
+import { FC, Fragment, ReactNode, useEffect, useRef, useState } from "react"
 
-import { Home, Inbox, LogOut, MoveLeft, Plus, UserPlus } from "lucide-react"
+import { Menu, Transition } from "@headlessui/react"
+import { ChevronDown, Home, Inbox, LogOut, MoveLeft, Plus, Settings, UserPlus } from "lucide-react"
 import { observer } from "mobx-react-lite"
 import { useTheme } from "next-themes"
 import toast from "react-hot-toast"
@@ -17,7 +18,7 @@ import useOutsideClickDetector from "@hooks/use-outside-click-detector"
 
 import UserAuthWrapper from "@wrappers/UserAuthWrapper"
 
-import { Tooltip } from "@servcy/ui"
+import { Avatar, Tooltip } from "@servcy/ui"
 
 const WORKSPACE_ACTION_LINKS = [
     {
@@ -55,6 +56,15 @@ const HOME_ACTION_LINKS = [
     },
 ]
 
+const USER_LINKS = (workspaceSlug: string) => [
+    {
+        key: "settings",
+        name: "Settings",
+        href: `/${workspaceSlug}/settings`,
+        icon: Settings,
+    },
+]
+
 interface INoWorkspaceWrapper {
     children: ReactNode
     header?: ReactNode
@@ -68,8 +78,9 @@ const DefaultWrapper: FC<INoWorkspaceWrapper> = observer((props) => {
     const {
         theme: { sidebarCollapsed, toggleSidebar },
     } = useApplication()
-    const { logOut } = useUser()
+    const { logOut, currentUser } = useUser()
     const { workspaces } = useWorkspace()
+    const { workspaceSlug } = useParams()
     const workspacesList = Object.values(workspaces ?? {})
     const ref = useRef<HTMLDivElement>(null)
 
@@ -125,6 +136,108 @@ const DefaultWrapper: FC<INoWorkspaceWrapper> = observer((props) => {
                                     lg:ml-0 ${sidebarCollapsed ? "w-[80px]" : "w-[280px]"}
                         `}
                     >
+                        <div className="flex items-center gap-x-3 gap-y-2 px-4 pt-4">
+                            <Menu as="div" className="relative h-full flex-grow truncate text-left">
+                                {({ open }) => (
+                                    <>
+                                        <Menu.Button className="group/menu-button h-full w-full truncate rounded-md text-sm font-medium text-custom-sidebar-text-200 hover:bg-custom-sidebar-background-80 focus:outline-none">
+                                            <div
+                                                className={`flex items-center  gap-x-2 truncate rounded p-1 ${
+                                                    sidebarCollapsed ? "justify-center" : "justify-between"
+                                                }`}
+                                            >
+                                                <div className="flex items-center gap-2 truncate">
+                                                    <div
+                                                        className={`relative grid h-6 w-6 flex-shrink-0 place-items-center uppercase ${
+                                                            !currentUser?.avatar &&
+                                                            "rounded bg-custom-primary-500 text-white"
+                                                        }`}
+                                                    >
+                                                        {currentUser?.avatar && currentUser.avatar !== "" ? (
+                                                            <Avatar
+                                                                name={currentUser?.display_name}
+                                                                src={currentUser?.avatar}
+                                                                className="!text-base"
+                                                                size={24}
+                                                                shape="square"
+                                                            />
+                                                        ) : (
+                                                            currentUser?.display_name?.charAt(0) ?? "..."
+                                                        )}
+                                                    </div>
+                                                    {!sidebarCollapsed && (
+                                                        <h4 className="truncate text-base font-medium text-custom-text-100">
+                                                            {currentUser?.display_name
+                                                                ? currentUser.display_name
+                                                                : "Loading..."}
+                                                        </h4>
+                                                    )}
+                                                </div>
+                                                {!sidebarCollapsed && (
+                                                    <ChevronDown
+                                                        className={`mx-1 hidden h-4 w-4 flex-shrink-0 group-hover/menu-button:block ${
+                                                            open ? "rotate-180" : ""
+                                                        } text-custom-sidebar-text-400 duration-300`}
+                                                    />
+                                                )}
+                                            </div>
+                                        </Menu.Button>
+                                        <Transition
+                                            as={Fragment}
+                                            enter="transition ease-out duration-100"
+                                            enterFrom="transform opacity-0 scale-95"
+                                            enterTo="transform opacity-100 scale-100"
+                                            leave="transition ease-in duration-75"
+                                            leaveFrom="transform opacity-100 scale-100"
+                                            leaveTo="transform opacity-0 scale-95"
+                                        >
+                                            <Menu.Items as={Fragment}>
+                                                <div className="fixed left-4 z-20 mt-1 flex w-full max-w-[19rem] origin-top-left flex-col rounded-md border-[0.5px] border-custom-sidebar-border-300 bg-custom-sidebar-background-100 shadow-custom-shadow-rg divide-y divide-custom-border-100 outline-none">
+                                                    <div className="flex max-h-96 flex-col items-start justify-start gap-2 overflow-y-scroll mb-2 px-4 vertical-scrollbar scrollbar-sm">
+                                                        <h6 className="sticky top-0 z-10 h-full w-full pt-3 pb-1 text-sm font-medium text-custom-sidebar-text-400 bg-custom-sidebar-background-100">
+                                                            {currentUser?.email}
+                                                        </h6>
+                                                    </div>
+                                                    <div className="flex w-full flex-col items-start justify-start gap-2 px-4 py-2 text-sm">
+                                                        {USER_LINKS(workspaceSlug?.toString() ?? "").map(
+                                                            (link, index) => (
+                                                                <Link
+                                                                    key={link.key}
+                                                                    href={link.href}
+                                                                    className="w-full"
+                                                                    onClick={() => {
+                                                                        if (index > 0) handleItemClick()
+                                                                    }}
+                                                                >
+                                                                    <Menu.Item
+                                                                        as="div"
+                                                                        className="flex items-center gap-2 rounded px-2 py-1 text-sm text-custom-sidebar-text-200 hover:bg-custom-sidebar-background-80 font-medium"
+                                                                    >
+                                                                        <link.icon className="h-4 w-4 flex-shrink-0" />
+                                                                        {link.name}
+                                                                    </Menu.Item>
+                                                                </Link>
+                                                            )
+                                                        )}
+                                                    </div>
+                                                    <div className="w-full px-4 py-2">
+                                                        <Menu.Item
+                                                            as="button"
+                                                            type="button"
+                                                            className="w-full flex items-center gap-2 rounded px-2 py-1 text-sm text-red-600 hover:bg-custom-sidebar-background-80 font-medium"
+                                                            onClick={handleSignOut}
+                                                        >
+                                                            <LogOut className="h-4 w-4 flex-shrink-0" />
+                                                            Sign out
+                                                        </Menu.Item>
+                                                    </div>
+                                                </div>
+                                            </Menu.Items>
+                                        </Transition>
+                                    </>
+                                )}
+                            </Menu>
+                        </div>
                         <div ref={ref} className="flex h-full w-full flex-col gap-y-4 mt-4">
                             <div className="flex flex-shrink-0 flex-col overflow-x-hidden px-4">
                                 {!sidebarCollapsed && (
