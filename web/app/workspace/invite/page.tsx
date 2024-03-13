ed3800c7b40df07976b1beb36ca9b9ebd2043e58
Megham Garg <gargmegham3@gmail.com>
2024-03-13 18:23:51 +0530

    Refactor import statements in workspace invitation page

--- web/app/workspace/invite/page.tsx
+++ web/app/workspace/invite/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
-import { useSearchParams, useRouter } from "next/navigation"
-import { Suspense } from 'react'
+import { useRouter, useSearchParams } from "next/navigation"
+
+import { Suspense } from "react"
 
 import { Boxes, Check, Share2, Star, User2, X } from "lucide-react"
 import { observer } from "mobx-react-lite"
@@ -21,7 +22,7 @@ import { Spinner } from "@servcy/ui"
 
 const workspaceService = new WorkspaceService()
 
-const WorkspaceInvitationPage = observer(() => {
+const InvitationList = observer(() => {
     const router = useRouter()
     const params = useSearchParams()
     const email = params.get("email")
@@ -68,65 +69,73 @@ const WorkspaceInvitationPage = observer(() => {
     }
 
     return (
-        <Suspense>
-            <NoSidebarWrapper>
-                <div className="flex h-full w-full flex-col items-center justify-center px-3">
-                    {invitationDetail ? (
+        <div className="flex h-full w-full flex-col items-center justify-center px-3">
+            {invitationDetail ? (
+                <>
+                    {error ? (
+                        <div className="flex w-full flex-col space-y-4 rounded border border-custom-border-200 bg-custom-background-100 px-4 py-8 text-center shadow-2xl md:w-1/3">
+                            <h2 className="text-xl uppercase">INVITATION NOT FOUND</h2>
+                        </div>
+                    ) : (
                         <>
-                            {error ? (
-                                <div className="flex w-full flex-col space-y-4 rounded border border-custom-border-200 bg-custom-background-100 px-4 py-8 text-center shadow-2xl md:w-1/3">
-                                    <h2 className="text-xl uppercase">INVITATION NOT FOUND</h2>
-                                </div>
-                            ) : (
+                            {invitationDetail.accepted ? (
                                 <>
-                                    {invitationDetail.accepted ? (
-                                        <>
-                                            <EmptySpace
-                                                title={`You are already a member of ${invitationDetail.workspace.name}`}
-                                                description="Your workspace is where you'll create projects, collaborate on your issues, and organize different streams of work in your Servcy account."
-                                            >
-                                                <EmptySpaceItem Icon={Boxes} title="Continue to Dashboard" href="/" />
-                                            </EmptySpace>
-                                        </>
-                                    ) : (
-                                        <EmptySpace
-                                            title={`You have been invited to ${invitationDetail.workspace.name}`}
-                                            description="Your workspace is where you'll create projects, collaborate on your issues, and organize different streams of work in your Servcy account."
-                                        >
-                                            <EmptySpaceItem Icon={Check} title="Accept" action={handleAccept} />
-                                            <EmptySpaceItem Icon={X} title="Ignore" action={handleReject} />
-                                        </EmptySpace>
-                                    )}
+                                    <EmptySpace
+                                        title={`You are already a member of ${invitationDetail.workspace.name}`}
+                                        description="Your workspace is where you'll create projects, collaborate on your issues, and organize different streams of work in your Servcy account."
+                                    >
+                                        <EmptySpaceItem Icon={Boxes} title="Continue to Dashboard" href="/" />
+                                    </EmptySpace>
                                 </>
+                            ) : (
+                                <EmptySpace
+                                    title={`You have been invited to ${invitationDetail.workspace.name}`}
+                                    description="Your workspace is where you'll create projects, collaborate on your issues, and organize different streams of work in your Servcy account."
+                                >
+                                    <EmptySpaceItem Icon={Check} title="Accept" action={handleAccept} />
+                                    <EmptySpaceItem Icon={X} title="Ignore" action={handleReject} />
+                                </EmptySpace>
                             )}
                         </>
-                    ) : error ? (
-                        <EmptySpace
-                            title="This invitation link is not active anymore."
-                            description="Your workspace is where you'll create projects, collaborate on your issues, and organize different streams of work in your Servcy account."
-                            link={{ text: "Or start from an empty project", href: "/" }}
-                        >
-                            {!currentUser ? (
-                                <EmptySpaceItem Icon={User2} title="Sign in to continue" href="/" />
-                            ) : (
-                                <EmptySpaceItem Icon={Boxes} title="Continue to Dashboard" href="/" />
-                            )}
-                            <EmptySpaceItem Icon={Star} title="Upvote us on ProductHunt" href="https://www.producthunt.com/posts/servcy" />
-                            <EmptySpaceItem
-                                Icon={Share2}
-                                title="Join our community on twitter"
-                                href="https://twitter.com/ContactServcy"
-                            />
-                        </EmptySpace>
-                    ) : (
-                        <div className="flex h-full w-full items-center justify-center">
-                            <Spinner />
-                        </div>
                     )}
+                </>
+            ) : error ? (
+                <EmptySpace
+                    title="This invitation link is not active anymore."
+                    description="Your workspace is where you'll create projects, collaborate on your issues, and organize different streams of work in your Servcy account."
+                    link={{ text: "Or start from an empty project", href: "/" }}
+                >
+                    {!currentUser ? (
+                        <EmptySpaceItem Icon={User2} title="Sign in to continue" href="/" />
+                    ) : (
+                        <EmptySpaceItem Icon={Boxes} title="Continue to Dashboard" href="/" />
+                    )}
+                    <EmptySpaceItem
+                        Icon={Star}
+                        title="Upvote us on ProductHunt"
+                        href="https://www.producthunt.com/posts/servcy"
+                    />
+                    <EmptySpaceItem
+                        Icon={Share2}
+                        title="Join our community on twitter"
+                        href="https://twitter.com/ContactServcy"
+                    />
+                </EmptySpace>
+            ) : (
+                <div className="flex h-full w-full items-center justify-center">
+                    <Spinner />
                 </div>
-            </NoSidebarWrapper>
-        </Suspense>
+            )}
+        </div>
     )
 })
+
+const WorkspaceInvitationPage = observer(() => (
+    <Suspense>
+        <NoSidebarWrapper>
+            <InvitationList />
+        </NoSidebarWrapper>
+    </Suspense>
+))
 
 export default WorkspaceInvitationPage
