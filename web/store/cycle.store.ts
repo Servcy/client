6bed1d7df58a1f49874c118b55d75f30b3480c43
Megham Garg <gargmegham3@gmail.com>
2024-03-14 16:59:47 +0530

    Fix currentProjectCycleIds logic

--- web/store/cycle.store.ts
+++ web/store/cycle.store.ts
@@ -105,8 +105,8 @@ export class CycleStore implements ICycleStore {
      */
     get currentProjectCycleIds() {
         const projectId = this.rootStore.app.router.projectId
-        if (!projectId || !this.fetchedMap[projectId]) return null
-        let allCycles = Object.values(this.cycleMap ?? {}).filter((c) => c?.project_id === projectId)
+        if (!projectId || !this.fetchedMap[projectId] || !this.cycleMap) return null
+        let allCycles = Object.values(this.cycleMap).filter((c) => c.project_id == projectId)
         allCycles = sortBy(allCycles, [(c) => c.sort_order])
         const allCycleIds = allCycles.map((c) => c.id)
         return allCycleIds
