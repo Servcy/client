e146a66df7408bed8697a6d6953c15b1503910bb
Megham Garg <gargmegham3@gmail.com>
2024-03-17 22:13:57 +0530

    BUGFI-7

--- web/components/workspace/settings/invitations-list-item.tsx
+++ web/components/workspace/settings/invitations-list-item.tsx
@@ -119,7 +119,7 @@ export const WorkspaceInvitationsListItem: FC<Props> = observer((props) => {
                                 return null
 
                             return (
-                                <CustomSelect.Option key={key} value={parseInt(key, 10)}>
+                                <CustomSelect.Option key={key} value={parseInt(key, 0)}>
                                     <>{ROLES[parseInt(key) as keyof typeof ROLES]}</>
                                 </CustomSelect.Option>
                             )
--- web/components/workspace/settings/members-list-item.tsx
+++ web/components/workspace/settings/members-list-item.tsx
@@ -171,7 +171,7 @@ export const WorkspaceMembersListItem: FC<Props> = observer((props) => {
                                 return null
 
                             return (
-                                <CustomSelect.Option key={key} value={parseInt(key, 10)}>
+                                <CustomSelect.Option key={key} value={parseInt(key, 0)}>
                                     <>{ROLES[parseInt(key) as keyof typeof ROLES]}</>
                                 </CustomSelect.Option>
                             )
