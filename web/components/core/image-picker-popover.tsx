f8ad1f073163bfdc37d4e1585f867f6eedb2734b
Megham Garg <gargmegham3@gmail.com>
2024-04-10 07:38:55 +0530

    Refactor image picker popover component

--- web/components/core/image-picker-popover.tsx
+++ web/components/core/image-picker-popover.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image"
-import { useParams } from "next/navigation"
 
 import React, { useCallback, useEffect, useRef, useState } from "react"
 
@@ -53,8 +52,6 @@ export const ImagePickerPopover: React.FC<Props> = observer((props) => {
     })
     // refs
     const ref = useRef<HTMLDivElement>(null)
-
-    const { workspaceSlug } = useParams()
     // store hooks
     const { currentWorkspace } = useWorkspace()
 
