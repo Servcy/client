7f800f2e67fae048d781a9eb543194e25295009f
Megham Garg <gargmegham3@gmail.com>
2024-04-15 13:44:55 +0530

    initial empty state closure in issue Create/Edit modal

--- web/components/issues/issue-modal/draft-issue-layout.tsx
+++ web/components/issues/issue-modal/draft-issue-layout.tsx
@@ -2,6 +2,7 @@ import { useParams, usePathname } from "next/navigation"
 
 import React, { useState } from "react"
 
+import { isEmpty } from "lodash"
 import { observer } from "mobx-react-lite"
 import toast from "react-hot-toast"
 
@@ -11,6 +12,8 @@ import { IssueFormRoot } from "@components/issues/issue-modal/form"
 import { useEventTracker } from "@hooks/store"
 
 import { IssueDraftService } from "@services/issue"
+
+import { isEmptyHtmlString } from "@helpers/string.helper"
 
 import type { TIssue } from "@servcy/types"
 
@@ -50,8 +53,29 @@ export const DraftIssueLayout: React.FC<DraftIssueProps> = observer((props) => {
     const { captureIssueEvent } = useEventTracker()
 
     const handleClose = () => {
-        if (changesMade) setIssueDiscardModal(true)
-        else onClose(false)
+        if (changesMade) {
+            Object.entries(changesMade).forEach(([key, value]) => {
+                const issueKey = key as keyof TIssue
+                if (value === null || value === undefined || value === "") delete changesMade[issueKey]
+                if (typeof value === "object" && isEmpty(value)) delete changesMade[issueKey]
+                if (Array.isArray(value) && value.length === 0) delete changesMade[issueKey]
+                if (issueKey === "project_id") delete changesMade.project_id
+                if (issueKey === "priority" && value && value === "none") delete changesMade.priority
+                if (
+                    issueKey === "description_html" &&
+                    changesMade.description_html &&
+                    isEmptyHtmlString(changesMade.description_html)
+                )
+                    delete changesMade.description_html
+            })
+            if (isEmpty(changesMade)) {
+                onClose(false)
+                setIssueDiscardModal(false)
+            } else setIssueDiscardModal(true)
+        } else {
+            onClose(false)
+            setIssueDiscardModal(false)
+        }
     }
 
     const handleCreateDraftIssue = async () => {
