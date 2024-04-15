d0d63d6060dd69ab0e1771441ec0462a8f693f7d
Megham Garg <gargmegham3@gmail.com>
2024-04-15 17:33:49 +0530

    Refactor active cycles page component in workspace

--- web/app/[workspaceSlug]/active-cycles/page.tsx
+++ web/app/[workspaceSlug]/active-cycles/page.tsx
@@ -5,39 +5,20 @@ import { useParams } from "next/navigation"
 import { useState } from "react"
 
 import { observer } from "mobx-react-lite"
-import { useTheme } from "next-themes"
 
 import { PageHead } from "@components/core"
 import { CycleCreateUpdateModal } from "@components/cycles"
-import { EmptyState, getEmptyStateImagePath } from "@components/empty-state"
 import { WorkspaceActiveCycleHeader } from "@components/headers"
-import { CycleModuleListLayout } from "@components/ui"
 import { WorkspaceActiveCycleRoot } from "@components/workspace"
 
-import { useCycle, useUser, useWorkspace } from "@hooks/store"
-
-import { CYCLE_EMPTY_STATE_DETAILS } from "@constants/empty-state"
-import { ERoles } from "@constants/iam"
+import { useWorkspace } from "@hooks/store"
 
 import { AppWrapper } from "@wrappers/app"
 
 const WorkspaceActiveCyclesPage = observer(() => {
     const [createModal, setCreateModal] = useState(false)
-    // theme
-    const { resolvedTheme } = useTheme()
-    // store hooks
-    const {
-        membership: { currentProjectRole },
-        currentUser,
-    } = useUser()
-    const { currentProjectCycleIds, loader } = useCycle()
     const { workspaceSlug } = useParams()
     const { currentWorkspace } = useWorkspace()
-    // derived values
-    const isLightMode = resolvedTheme ? resolvedTheme === "light" : currentUser?.theme.theme === "light"
-    const EmptyStateImagePath = getEmptyStateImagePath("onboarding", "cycles", isLightMode)
-    const totalCycles = currentProjectCycleIds?.length ?? 0
-    const isEditingAllowed = currentProjectRole !== undefined && currentProjectRole >= ERoles.MEMBER
     const pageTitle = currentWorkspace?.name ? `${currentWorkspace?.name} - Active Cycles` : undefined
 
     if (!workspaceSlug) return null
@@ -51,21 +32,7 @@ const WorkspaceActiveCyclesPage = observer(() => {
                     isOpen={createModal}
                     handleClose={() => setCreateModal(false)}
                 />
-                {loader ? (
-                    <CycleModuleListLayout />
-                ) : totalCycles === 0 ? (
-                    <div className="h-full place-items-center">
-                        <EmptyState
-                            title={CYCLE_EMPTY_STATE_DETAILS["active"].title}
-                            description={CYCLE_EMPTY_STATE_DETAILS["active"].description}
-                            image={EmptyStateImagePath}
-                            size="lg"
-                            disabled={!isEditingAllowed}
-                        />
-                    </div>
-                ) : (
-                    <WorkspaceActiveCycleRoot workspaceSlug={workspaceSlug.toString()} />
-                )}
+                <WorkspaceActiveCycleRoot workspaceSlug={workspaceSlug.toString()} />
             </div>
         </AppWrapper>
     )
