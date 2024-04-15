443ce0aab272e512257b62d7096bfb2f2cf3a336
Megham Garg <gargmegham3@gmail.com>
2024-04-15 10:56:39 +0530

    Refactor ProjectArchivedIssueDetailsHeader component to update Breadcrumbs and fix CSS class names

--- web/components/headers/project-archived-issue-details.tsx
+++ web/components/headers/project-archived-issue-details.tsx
@@ -17,7 +17,7 @@ import { IssueArchiveService } from "@services/issue"
 import { renderEmoji } from "@helpers/emoji.helper"
 
 import { TIssue } from "@servcy/types"
-import { Breadcrumbs, LayersIcon } from "@servcy/ui"
+import { ArchiveIcon, Breadcrumbs, LayersIcon } from "@servcy/ui"
 
 const issueArchiveService = new IssueArchiveService()
 
@@ -39,7 +39,7 @@ export const ProjectArchivedIssueDetailsHeader: FC = observer(() => {
     )
 
     return (
-        <div className="relative z-10 flex h-[3.75rem] w-full flex-shrink-0 flex-row items-center justify-between gap-x-2 gap-y-4 border-b border-custom-border-200 bg-custom-sidebar-background-100 p-4">
+        <div className="relative z-10 flex h-14 w-full flex-shrink-0 flex-row items-center justify-between gap-x-2 gap-y-4 bg-custom-sidebar-background-100 p-4">
             <div className="flex w-full flex-grow items-center gap-2 overflow-ellipsis whitespace-nowrap">
                 <SidebarHamburgerToggle />
                 <div>
@@ -64,13 +64,22 @@ export const ProjectArchivedIssueDetailsHeader: FC = observer(() => {
                                 />
                             }
                         />
-
                         <Breadcrumbs.BreadcrumbItem
                             type="text"
                             link={
                                 <BreadcrumbLink
-                                    href={`/${workspaceSlug}/projects/${projectId}/archives`}
-                                    label="Archived issues"
+                                    href={`/${workspaceSlug}/projects/${projectId}/archives/issues`}
+                                    label="Archives"
+                                    icon={<ArchiveIcon className="h-4 w-4 text-custom-text-300" />}
+                                />
+                            }
+                        />
+                        <Breadcrumbs.BreadcrumbItem
+                            type="text"
+                            link={
+                                <BreadcrumbLink
+                                    href={`/${workspaceSlug}/projects/${projectId}/archives/issues`}
+                                    label="Issues"
                                     icon={<LayersIcon className="h-4 w-4 text-custom-text-300" />}
                                 />
                             }
