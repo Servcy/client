e9d17c6d8e8aa7e9c9c9f167cebc99970ca9866f
Megham Garg <gargmegham3@gmail.com>
2024-04-26 14:51:17 +0530

    ProjectCostAnalysis base component

---
+++ web/app/[workspaceSlug]/projects/[projectId]/budget/page.tsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { useParams } from "next/navigation"
+
+import { PageHead } from "@components/core"
+
+import { useProject } from "@hooks/store"
+
+import { AppWrapper } from "@wrappers/app"
+
+const ProjectCostAnalysis = () => {
+    const { projectId } = useParams()
+    const { getProjectById } = useProject()
+    const project = projectId ? getProjectById(projectId.toString()) : undefined
+    const pageTitle = project?.name ? `${project?.name} - Cost Analysis` : undefined
+    return (
+        <AppWrapper header={<div />} withProjectWrapper>
+            <PageHead title={pageTitle} />
+            <div className="h-full w-full flex flex-col">{/*  */}</div>
+        </AppWrapper>
+    )
+}
+
+export default ProjectCostAnalysis
